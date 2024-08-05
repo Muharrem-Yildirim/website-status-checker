083e14a4ede2c7321ae289dc59224493141a875b
Muharrem-Yildirim <muharremyildirim303@gmail.com>
2024-08-05 22:07:53 +0300

    Update register-host-schema.ts

--- src/zod/register-host-schema.ts
+++ src/zod/register-host-schema.ts
@@ -2,4 +2,10 @@ import z from "zod";
 
 export const registerHostValidation = z.object({
   hostname: z.string(),
+  notifyOptions: z
+    .object({
+      mail: z.boolean().optional().default(false),
+      telegram: z.boolean().optional().default(false),
+    })
+    .strict(),
 });
