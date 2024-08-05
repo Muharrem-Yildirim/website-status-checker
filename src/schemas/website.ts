427533b86acfc8285c5a8e368333fa9f554bf672
Muharrem-Yildirim <muharremyildirim303@gmail.com>
2024-08-05 22:08:26 +0300

    notifyOptions and protocol added to website schema

--- src/schemas/website.ts
+++ src/schemas/website.ts
@@ -8,6 +8,11 @@ export enum Plans {
 export interface IWebsite {
   plan: Plans;
   hostname: string;
+  protocol: "http" | "https";
+  notifyOptions: {
+    mail: boolean;
+    telegram: boolean;
+  };
   lastChecked: Date;
 }
 
@@ -15,6 +20,11 @@ const websiteSchema = new Schema<IWebsite>(
   {
     plan: { type: String, required: true, default: Plans.FREE },
     hostname: { type: String, required: true, unique: true },
+    protocol: { type: String, required: true, default: "http" },
+    notifyOptions: {
+      mail: { type: Boolean, required: true, default: false },
+      telegram: { type: Boolean, required: true, default: false },
+    },
     lastChecked: { type: Date, required: false, default: null },
   },
   {
