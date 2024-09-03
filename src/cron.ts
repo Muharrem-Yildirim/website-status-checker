8001626e1292a05f475cd7ed42635d4cedf6054d
Muharrem-Yildirim <muharremyildirim303@gmail.com>
2024-09-03 17:11:37 +0300

    code refactored

--- src/cron.ts
+++ src/cron.ts
@@ -1,31 +1,33 @@
 import cron from "node-cron";
-import Website, { Plans } from "./schemas/website";
+import Website, { Plan } from "./schemas/website";
 import { ping } from "./services/checker-service";
 
-export function initCrons() {
-  cron.schedule("* * * * *", async function () {
-    const FREE_PLAN_WEBSITES = await Website.find({
-      lastChecked: {
-        $lt: new Date(
-          Date.now() -
-            1000 * (parseInt(process.env.CHECK_INTERVAL) || 60 * 60) * 2
-        ),
-      },
-      plan: Plans.FREE,
-    });
-
-    const PAID_PLAN_WEBSITES = await Website.find({
-      lastChecked: {
-        $lt: new Date(
-          Date.now() - 1000 * (parseInt(process.env.CHECK_INTERVAL) || 60 * 60)
-        ),
-      },
-      plan: Plans.PAID,
-    });
-
-    ping(FREE_PLAN_WEBSITES);
-    ping(PAID_PLAN_WEBSITES);
+async function job() {
+  const FREE_PLAN_WEBSITES = await Website.find({
+    lastCheck: {
+      $lt: new Date(
+        Date.now() -
+          1000 * (parseInt(process.env.CHECK_INTERVAL) || 60 * 60) * 2
+      ),
+    },
+    plan: Plan.FREE,
   });
+
+  const PAID_PLAN_WEBSITES = await Website.find({
+    lastCheck: {
+      $lt: new Date(
+        Date.now() - 1000 * (parseInt(process.env.CHECK_INTERVAL) || 60 * 60)
+      ),
+    },
+    plan: Plan.PAID,
+  });
+
+  ping(FREE_PLAN_WEBSITES);
+  ping(PAID_PLAN_WEBSITES);
+}
+
+export function initCrons() {
+  cron.schedule("* * * * *", job);
 
   console.log("Crons initialized");
 }
