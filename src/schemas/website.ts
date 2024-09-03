4cf76a883b0c69becf299b205eee015041295616
Muharrem-Yildirim <muharremyildirim303@gmail.com>
2024-09-03 17:11:01 +0300

    Update website.ts

--- src/schemas/website.ts
+++ src/schemas/website.ts
@@ -1,36 +1,56 @@
 import { Schema, model } from "mongoose";
 
-export enum Plans {
+export enum Plan {
   FREE = "free",
   PAID = "paid",
 }
 
 export interface IWebsite {
-  plan: Plans;
+  plan: Plan;
+  ownerIdentifier: string;
   hostname: string;
   protocol: "http" | "https";
   notifyOptions: {
     mail: boolean;
     telegram: boolean;
   };
-  lastChecked: Date;
+  checkCount: number;
+  failedCheckCount: number;
+  lastCheck: Date;
+  isActive: boolean;
+  logs: Schema.Types.ObjectId[];
 }
 
 const websiteSchema = new Schema<IWebsite>(
   {
-    plan: { type: String, required: true, default: Plans.FREE },
-    hostname: { type: String, required: true, unique: true },
+    plan: { type: String, required: true, default: Plan.FREE },
+    ownerIdentifier: { type: String, required: true },
+    hostname: { type: String, required: true },
     protocol: { type: String, required: true, default: "http" },
     notifyOptions: {
-      mail: { type: Boolean, required: true, default: false },
+      email: { type: Boolean, required: true, default: false },
       telegram: { type: Boolean, required: true, default: false },
     },
-    lastChecked: { type: Date, required: false, default: null },
+    checkCount: { type: Number, required: true, default: 0 },
+    failedCheckCount: { type: Number, required: true, default: 0 },
+    lastCheck: { type: Date, required: false, default: null },
+    isActive: { type: Boolean, required: true, default: false },
   },
   {
     timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
   }
 );
+
+websiteSchema.index({ ownerIdentifier: 1, hostname: 1 }, { unique: true });
+websiteSchema.virtual("logs", {
+  ref: "Log",
+  localField: "_id",
+  foreignField: "website",
+  justOne: false,
+});
 
 const Website = model<IWebsite>("Website", websiteSchema);
 
