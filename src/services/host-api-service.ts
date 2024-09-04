9968d2d93ff2dc8d504d63f4f038248cacf05369
Muharrem-Yildirim <muharremyildirim303@gmail.com>
2024-09-04 21:53:08 +0300

    code formatted

--- src/services/host-api-service.ts
+++ src/services/host-api-service.ts
@@ -2,179 +2,195 @@ import { StatusCodes } from "http-status-codes";
 import Host, { Plan } from "../schemas/host";
 
 const getHosts = (req: Request & { query: { filter?: string } }, res) => {
-  const filter = JSON.parse((req.query?.filter as string) ?? "{}");
+	const filter = JSON.parse((req.query?.filter as string) ?? "{}");
 
-  Host.find(filter)
-    .populate({
-      path: "logs",
-      options: { sort: { createdAt: -1 }, limit: 120 },
-    })
-    .then((hosts) => {
-      res.json({
-        success: true,
-        data: hosts,
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        success: false,
-        message: "Error fetching hosts.",
-        details: err,
-      });
-    });
+	Host.find(filter)
+		.populate({
+			path: "logs",
+			options: { sort: { createdAt: -1 }, limit: 120 },
+		})
+		.then((hosts) => {
+			res.json({
+				success: true,
+				data: hosts,
+			});
+		})
+		.catch((err) => {
+			res.status(500).json({
+				success: false,
+				message: "Error fetching hosts.",
+				details: err,
+			});
+		});
 };
 
 const getHostById = (
-  req: Request & {
-    query: { ownerIdentifier?: string };
-    params: { id: string };
-  },
-  res
+	req: Request & {
+		query: { ownerIdentifier?: string; page?: string; limit?: string };
+		params: { id: string };
+	},
+	res
 ) => {
-  const ownerIdentifier = (req.query?.ownerIdentifier as string) ?? "";
+	const ownerIdentifier = req.query?.ownerIdentifier ?? "";
+	const _page = parseInt(req.query?.page as string) || 0;
+	let _limit = parseInt(req.query?.limit as string) || 120;
 
-  Host.findOne({ ownerIdentifier, _id: req.params.id })
-    .populate({
-      path: "logs",
-      options: { sort: { createdAt: -1 }, limit: 120 },
-    })
+	if (_limit > 120) _limit = 120;
 
-    .then((host) => {
-      if (!host) throw new Error("Host not found");
+	const _skip = _page * _limit;
 
-      res.json({
-        success: true,
-        data: host,
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        success: false,
-        message: "Error fetching hosts.",
-        details: err,
-      });
-    });
+	let filter: { _id: string; ownerIdentifier?: string } = {
+		_id: req.params.id,
+	};
+
+	if (ownerIdentifier) filter = { ownerIdentifier, _id: req.params.id };
+
+	Host.findOne(filter)
+		.populate({
+			path: "logs",
+			options: {
+				sort: { createdAt: -1 },
+				skip: _skip,
+				limit: _limit,
+			},
+		})
+
+		.then((host) => {
+			if (!host) throw new Error("Host not found");
+
+			res.json({
+				success: true,
+				data: host,
+			});
+		})
+		.catch((err) => {
+			res.status(500).json({
+				success: false,
+				message: "Error fetching hosts.",
+				details: err,
+			});
+		});
 };
 
 const saveHost = async (
-  req: Request & { validatedBody: any; query: { ownerIdentifier?: string } },
-  res
+	req: Request & { validatedBody: any; query: { ownerIdentifier?: string } },
+	res
 ) => {
-  console.log("Received request:", req.body);
-  const { hostname, ownerIdentifier, plan } = req.validatedBody;
+	console.log("Received request:", req.body);
+	const { hostname, ownerIdentifier, plan } = req.validatedBody;
 
-  console.log(req.validatedBody);
+	console.log(req.validatedBody);
 
-  const isAlreadyExists = await Host.findOne({
-    ownerIdentifier,
-    hostname,
-  });
+	const isAlreadyExists = await Host.findOne({
+		ownerIdentifier,
+		hostname,
+	});
 
-  if (isAlreadyExists) {
-    return res.status(403).json({
-      success: false,
-      message: "Host already exists.",
-    });
-  }
+	if (isAlreadyExists) {
+		return res.status(403).json({
+			success: false,
+			message: "Host already exists.",
+		});
+	}
 
-  var limit = 5;
+	var limit = 5;
 
-  if (plan == Plan.PAID) {
-    limit = 25;
-  }
+	if (plan == Plan.PAID) {
+		limit = 25;
+	}
 
-  const count = await Host.countDocuments({
-    ownerIdentifier,
-  });
+	const count = await Host.countDocuments({
+		ownerIdentifier,
+	});
 
-  if (count >= limit) {
-    return res.status(403).json({
-      success: false,
-      message: "You have reached the limit of hosts.",
-    });
-  }
+	if (count >= limit) {
+		return res.status(403).json({
+			success: false,
+			message: "You have reached the limit of hosts.",
+		});
+	}
 
-  Host.create({ ...req.validatedBody })
-    .then((data) => {
-      res.json({
-        success: true,
-        message: `Successfully registered ${hostname}.`,
-        data,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        success: false,
-        message: "Error registering host.",
-        details: err.message,
-      });
-    });
+	Host.create({ ...req.validatedBody })
+		.then((data) => {
+			res.json({
+				success: true,
+				message: `Successfully registered ${hostname}.`,
+				data,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				success: false,
+				message: "Error registering host.",
+				details: err.message,
+			});
+		});
 };
 
 const updateHost = async (
-  req: Request & { validatedBody: any; params: { id: string } },
-  res
+	req: Request & { validatedBody: any; params: { id: string } },
+	res
 ) => {
-  console.log("Received request:", req.body);
-  const { hostname, ownerIdentifier } = req.validatedBody;
-  const { id } = req.params;
+	console.log("Received request:", req.body);
+	const { hostname, ownerIdentifier } = req.validatedBody;
+	const { id } = req.params;
 
-  Host.updateOne(
-    { ownerIdentifier, _id: id },
-    { ...req.validatedBody },
-    {
-      returnDocument: "after",
-    }
-  )
-    .then((data) => {
-      res.json({
-        success: true,
-        message: `Successfully registered ${hostname}.`,
-        data,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        success: false,
-        message:
-          err.code === 11000
-            ? "Host already exists."
-            : "Error registering host.",
-        details: err,
-      });
-    });
+	Host.updateOne(
+		{ ownerIdentifier, _id: id },
+		{ ...req.validatedBody },
+		{
+			returnDocument: "after",
+		}
+	)
+		.then((data) => {
+			res.json({
+				success: true,
+				message: `Successfully registered ${hostname}.`,
+				data,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				success: false,
+				message:
+					err.code === 11000
+						? "Host already exists."
+						: "Error registering host.",
+				details: err,
+			});
+		});
 };
 
 const deleteHost = (
-  req: Request & {
-    query: { ownerIdentifier?: string };
-    params: { id: string };
-  },
-  res
+	req: Request & {
+		query: { ownerIdentifier?: string };
+		params: { id: string };
+	},
+	res
 ) => {
-  const ownerIdentifier = (req.query?.ownerIdentifier as string) ?? "";
-  const { id } = req.params;
+	const ownerIdentifier = (req.query?.ownerIdentifier as string) ?? "";
+	const { id } = req.params;
 
-  Host.findOneAndDelete({ ownerIdentifier, _id: id })
-    .then((host) => {
-      if (!host) {
-        return res.status(404).json({
-          success: false,
-          message: "Hostname not found.",
-        });
-      }
+	Host.findOneAndDelete({ ownerIdentifier, _id: id })
+		.then((host) => {
+			if (!host) {
+				return res.status(404).json({
+					success: false,
+					message: "Hostname not found.",
+				});
+			}
 
-      res.status(StatusCodes.NO_CONTENT).json({});
-    })
-    .catch((err) => {
-      res.status(500).json({
-        success: false,
-        message: "Error unregistering host.",
-        details: err,
-      });
-    });
+			res.status(StatusCodes.NO_CONTENT).json({});
+		})
+		.catch((err) => {
+			res.status(500).json({
+				success: false,
+				message: "Error unregistering host.",
+				details: err,
+			});
+		});
 };
 
 export { getHosts, getHostById, saveHost, updateHost, deleteHost };
