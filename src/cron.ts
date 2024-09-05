efe926cb81d31bc304bcd1c0d33b3dfde3c7fb23
Muharrem-Yildirim <muharremyildirim303@gmail.com>
2024-09-05 13:19:46 +0300

    spaces converted to tabs

--- src/cron.ts
+++ src/cron.ts
@@ -3,39 +3,42 @@ import Host, { Plan } from "./schemas/host";
 import { ping } from "./services/checker-service";
 
 async function job() {
-  const query = {
-    $or: [
-      {
-        lastCheck: {
-          $lt: new Date(
-            Date.now() -
-              1000 * (parseInt(process.env.CHECK_INTERVAL) || 60 * 60) * 2
-          ),
-        },
-      },
-      {
-        lastCheck: null,
-      },
-    ],
-    isActive: true,
-  };
+	const query = {
+		$or: [
+			{
+				lastCheck: {
+					$lt: new Date(
+						Date.now() -
+							1000 *
+								(parseInt(process.env.CHECK_INTERVAL) ||
+									60 * 60) *
+								2
+					),
+				},
+			},
+			{
+				lastCheck: null,
+			},
+		],
+		isActive: true,
+	};
 
-  const FREE_PLAN_WEBSITES = await Host.find({
-    ...query,
-    plan: Plan.FREE,
-  });
+	const FREE_PLAN_WEBSITES = await Host.find({
+		...query,
+		plan: Plan.FREE,
+	});
 
-  const PAID_PLAN_WEBSITES = await Host.find({
-    ...query,
-    plan: Plan.PAID,
-  });
+	const PAID_PLAN_WEBSITES = await Host.find({
+		...query,
+		plan: Plan.PAID,
+	});
 
-  ping(FREE_PLAN_WEBSITES);
-  ping(PAID_PLAN_WEBSITES);
+	ping(FREE_PLAN_WEBSITES);
+	ping(PAID_PLAN_WEBSITES);
 }
 
 export function initCrons() {
-  cron.schedule("* * * * *", job);
+	cron.schedule("* * * * *", job);
 
-  console.log("Crons initialized");
+	console.log("Crons initialized");
 }
