3d5c959be0e43de4e77f350fa5a9adf0fa23169d
Muharrem-Yildirim <muharremyildirim303@gmail.com>
2024-09-06 21:34:00 +0300

    changed fetch to axios, added timeout and variable renamed

--- src/services/checker-service.ts
+++ src/services/checker-service.ts
@@ -1,55 +1,59 @@
+import axios from "axios";
 import { LogTypes } from "../schemas/log";
 import { log } from "./log-service";
 
 export async function ping(hosts) {
-  for await (const WEBSITE of hosts) {
-    fetch(`${WEBSITE.protocol}://${WEBSITE.hostname}`)
-      .then((res) => {
-        if (res.ok) {
-          log(
-            WEBSITE,
-            LogTypes.INFO,
-            `Successfully connected to ${WEBSITE.hostname}.`
-          );
-        } else {
-          log(
-            WEBSITE,
-            LogTypes.ERROR,
-            `Error while connecting to ${WEBSITE.hostname}, response code is [${res.status} ${res.statusText}].`
-          );
-        }
-      })
-      .catch(async (error) => {
-        log(
-          WEBSITE,
-          LogTypes.ERROR,
-          `Error while connecting to ${WEBSITE.hostname}, message is [${error.message}].`
-        );
+	for await (const host of hosts) {
+		axios
+			.get(`${host.protocol}://${host.hostname}`, {
+				timeout: 5000,
+			})
+			.then((res) => {
+				if (res.ok) {
+					log(
+						host,
+						LogTypes.INFO,
+						`Successfully connected to ${host.hostname}.`
+					);
+				} else {
+					log(
+						host,
+						LogTypes.ERROR,
+						`Error while connecting to ${host.hostname}, response code is [${res.status} ${res.statusText}].`
+					);
+				}
+			})
+			.catch(async (error) => {
+				log(
+					host,
+					LogTypes.ERROR,
+					`Error while connecting to ${host.hostname}, message is [${error.message}].`
+				);
 
-        await WEBSITE.updateOne({
-          $where: {
-            hostname: WEBSITE.hostname,
-          },
-          $set: {
-            lastCheck: new Date(),
-          },
-          $inc: {
-            failedCheckCount: 1,
-          },
-        });
-      })
-      .finally(async () => {
-        await WEBSITE.updateOne({
-          $where: {
-            hostname: WEBSITE.hostname,
-          },
-          $set: {
-            lastCheck: new Date(),
-          },
-          $inc: {
-            checkCount: 1,
-          },
-        });
-      });
-  }
+				await host.updateOne({
+					$where: {
+						hostname: host.hostname,
+					},
+					$set: {
+						lastCheck: new Date(),
+					},
+					$inc: {
+						failedCheckCount: 1,
+					},
+				});
+			})
+			.finally(async () => {
+				await host.updateOne({
+					$where: {
+						hostname: host.hostname,
+					},
+					$set: {
+						lastCheck: new Date(),
+					},
+					$inc: {
+						checkCount: 1,
+					},
+				});
+			});
+	}
 }
