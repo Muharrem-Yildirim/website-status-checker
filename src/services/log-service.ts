b220674e1c8063c408558341f42318b4717cb333
Muharrem-Yildirim <muharremyildirim303@gmail.com>
2024-09-07 19:14:10 +0300

    code refactored

--- src/services/log-service.ts
+++ src/services/log-service.ts
@@ -13,41 +13,60 @@ async function log(host, type, message) {
 		createdAt: -1,
 	});
 
-	if (
-		type == LogTypes.ERROR &&
-		(!lastLog || lastLog.isUp) //status is changed
-	) {
-		Object.keys(notificatorMap).forEach(async (target) => {
-			if (
-				host.notifyOptions[target]?.isActive &&
-				host.notifyOptions[target]?.target != "" &&
-				host.notifyOptions[target]?.target != null
-			) {
-				notificatorMap[target]()
-					.then((notificator) => {
-						notificator.default.notify(
-							`[${type}] ${message}`,
-							host.hostname,
-							host.notifyOptions[target]?.target
-						);
+	const isStatusChanged = !lastLog || lastLog.isUp !== log.isUp;
 
-						console.log(
-							"Notified target:",
-							target,
-							"Options",
-							host.notifyOptions[target]
-						);
-					})
-					.catch(console.error);
-			}
-		});
+	if (isStatusChanged) {
+		broadcast(host, type, message);
 	} else {
 		console.log("Skipped notification for ", host.hostname);
 	}
 
-	console.log(`[${new Date().toUTCString()}] [${type}] ${message}`);
+	console.log(
+		`[${new Date().toUTCString()}] [${type}] ${
+			message ?? "Successfully connected."
+		}`,
+		host.hostname
+	);
 
 	if (global.loggingEnabled) await log.save();
+}
+
+function broadcast(host, type, message) {
+	Object.keys(notificatorMap).forEach(async (target) => {
+		if (
+			host.notifyOptions[target]?.isActive &&
+			host.notifyOptions[target]?.target != "" &&
+			host.notifyOptions[target]?.target != null
+		) {
+			const _subject =
+				type === LogTypes.ERROR
+					? `❌ Your Site is Down - ${host.hostname}`
+					: `✅ Your Site is Up - ${host.hostname}`;
+
+			const _message =
+				type === LogTypes.ERROR
+					? `Hi, your site ${host.hostname} is down. The response was: [${message}].`
+					: `Hi, your site ${host.hostname} is up again.`;
+
+			notificatorMap[target]()
+				.then((notificator) => {
+					notificator.default.notify(
+						_subject,
+						_message,
+						host.hostname,
+						host.notifyOptions[target]?.target
+					);
+
+					console.log(
+						"Notified target:",
+						target,
+						"Options",
+						host.notifyOptions[target]
+					);
+				})
+				.catch(console.error);
+		}
+	});
 }
 
 export { log };
